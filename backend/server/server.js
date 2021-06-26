d9114b5a4be7e9bce82b715bbc03ad882aee000e
crown-ships <85935214+crown-ships@users.noreply.github.com>
2021-06-26 12:17:06 +0530

    Update server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,5 +1,6 @@
 const app = require('../app');
 
-app.listen(4000, () => console.log("server is up and running"));
+const port = process.env.PORT || 4000
+app.listen(port, () => console.log("server is up and running"));
 
 module.exports = app;
