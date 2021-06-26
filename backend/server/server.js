a3cbfa9df47425c79936b871dc41a5a6887e14e2
crown-ships <crowntechgit@gmail.com>
2021-06-26 19:31:02 +0530

    Update server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const port = process.env.PORT || 4000
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    const path = require("path");
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+  // Step 1:
+    app.use(express.static(path.resolve(__dirname, "./client/build")));
+    // Step 2:
+    app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    });
 }
 
 app.listen(port, () => console.log("server is up and running"));
