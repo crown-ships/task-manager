c9f1a2cbc25e3f05e39eb30dced609dc6b419ac1
crown-ships <crowntechgit@gmail.com>
2021-06-27 13:10:16 +0530

    path

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,5 +1,6 @@
 const app = require('../app');
 const express = require('express');
+const path = require("path");
 const port = process.env.PORT || 4000
 
 if (process.env.NODE_ENV === "production") {
