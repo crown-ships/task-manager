9b006e23b381618cf1110eecf245b486f855322f
crown-ships <crowntechgit@gmail.com>
2021-07-02 02:30:34 +0530

    import

--- backend/client/src/components/sections/projects/forms/registerTaskForm.js
+++ backend/client/src/components/sections/projects/forms/registerTaskForm.js
@@ -4,7 +4,9 @@ import TextField from '@material-ui/core/TextField';
 import Button from "../../../controls/Button";
 import Input from "../../../controls/Input";
 import { useForm, Form } from '../../useForm';
-
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 const initialFValues = {
     taskName: '',
