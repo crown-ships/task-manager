9294bc04ee7a64ee060cacd32b893c0ff05973e8
crown-ships <crowntechgit@gmail.com>
2021-07-06 20:04:46 +0530

    Update ProjectDetails.js

--- backend/client/src/components/sections/projects/tables/projects/ProjectDetails.js
+++ backend/client/src/components/sections/projects/tables/projects/ProjectDetails.js
@@ -1,13 +1,30 @@
 import Button from '@material-ui/core/Button';
 import {Link} from "react-router-dom"
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logoutUser } from "../../../actions/authActions";
+import { getAllCompanies, updateCompany } from "../../../actions/companyActions";
+import { getAllProjects, deleteProject, updateProject, registerProject } from "../../../actions/projectActions";
+import { getAllTasks, deleteTask, updateTask, registerTask ,updateAllTasks } from "../../../actions/taskActions";
+import { getAllFeatures, deleteFeature, updateFeature, registerFeature, updateAllFeatures } from "../../../actions/featureActions";
 
-
-export default function ProjectDetails(props) {
+const ProjectDetails = (props) => {
  console.log(props.location.props.projectID);
  return (
-   <>
    <Button component={Link} to={{pathname: "/projects"}}>{props.location.props.projectID}</Button>
-   </>
  );
 
 }
+
+ProjectsPage.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+export default connect(
+  mapStateToProps,
+  { updateAllFeatures,updateAllTasks, updateCompany, logoutUser, getAllProjects, getAllCompanies, deleteProject, updateProject, registerProject,getAllFeatures, deleteFeature, updateFeature, registerFeature,getAllTasks, deleteTask, updateTask, registerTask }
+)(withRouter(ProjectsPage));
