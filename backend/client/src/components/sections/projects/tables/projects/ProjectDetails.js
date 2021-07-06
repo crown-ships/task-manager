f63c264b1aa6d46a13e943073c20ed46d19b6915
crown-ships <crowntechgit@gmail.com>
2021-07-06 19:40:24 +0530

    Update ProjectDetails.js

--- backend/client/src/components/sections/projects/tables/projects/ProjectDetails.js
+++ backend/client/src/components/sections/projects/tables/projects/ProjectDetails.js
@@ -1,7 +1,13 @@
-
+import Button from '@material-ui/core/Button';
 
 
 
 export default function ProjectDetails(props) {
- return (props.location.props.projectID);
+ console.log(props.location.props.projectID);
+ return (
+   <>
+   <Button component={Link} to={{pathname: "/projects"}}>{props.location.props.projectID}</Button>
+   </>
+ );
+
 }
