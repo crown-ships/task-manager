ad14115e9deb00868a256a40786e4e05198e351c
crown-ships <crowntechgit@gmail.com>
2021-07-08 22:12:05 +0530

    Update ProjectDetails.js

--- backend/client/src/components/sections/projects/tables/projects/ProjectDetails.js
+++ backend/client/src/components/sections/projects/tables/projects/ProjectDetails.js
@@ -1,6 +1,7 @@
 import React  from 'react';
 import {Link} from "react-router-dom"
 import clsx from 'clsx';
+import Input from "../../../../controls/Input";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { makeStyles } from '@material-ui/core/styles';
