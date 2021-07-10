796947ccbf688c3deef09f3b08254032e2ec1c75
crown-ships <crowntechgit@gmail.com>
2021-07-10 21:43:08 +0530

    Update routes.js

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -47,7 +47,6 @@ router.get('/getProjects', userController.allowIfLoggedin, userController.grantA
 
 router.get('/getFilteredProjects', userController.allowIfLoggedin, userController.grantAccess('readAny', 'project'),  projectController.getFilteredProjects);
 
-getFilteredProjects
 
 //------------------FEATURES
 router.post('/newFeature', userController.allowIfLoggedin, userController.grantAccess('create', 'feature'),featureController.addNew);
